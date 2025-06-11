6beea9a8c1fd2d5b1956e58620e10b1958aa8898
PRAVESH GREWAL <118418011+praveshgrewal@users.noreply.github.com>
2025-06-11 16:42:02 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+ALLOWED_HOSTES =['django-shop.onrender.com]
+
 STATIC_URL = 'static/'
 STATICSFILES_DIRS = [
     BASE_DIR / 'static',
